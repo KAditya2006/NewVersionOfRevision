7c2f2285b90967c8e33ceaf5bbe2a4a638f18a4e
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-02 09:50:43 +0530

    Solved Second chapter fourth problem

---
+++ JSPracticeSet/Chapter2/problem4.js
@@ -0,0 +1,11 @@
+//Write a javascript program to find wether a number is divisible by 2 and 3.
+
+let num = 24;
+
+if(num % 2 === 0 && num % 3 ===0){
+    console.log("The number is divisible by both 2 and 3");
+} else {
+    console.log("The number is not divisible by both 2 and 3");
+}
+
+//output: The number is divisible by both 2 and 3
