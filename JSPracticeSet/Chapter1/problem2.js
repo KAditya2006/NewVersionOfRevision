0e263558c65bb91b01acecf81a94b55dcbb37206
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-02 09:10:22 +0530

    Solved first chapter second problem

---
+++ JSPracticeSet/Chapter1/problem2.js
@@ -0,0 +1,15 @@
+//Use typeof operator to find the datatype of the string in last question.
+let name = "Aditya";
+let age = 19;
+
+let result = name + age;
+console.log(result);
+
+
+
+
+console.log(typeof result);
+
+
+/*output: Aditya19
+string*/
