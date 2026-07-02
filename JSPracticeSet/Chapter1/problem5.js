f08e726b328e4d50e6ee68e7bd3cdc57ad31b21b
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-02 09:21:09 +0530

    Solved first chapter fifth problem

---
+++ JSPracticeSet/Chapter1/problem5.js
@@ -0,0 +1,20 @@
+//Write a js program to create a word meaning dictionary of 5 word.
+
+const dict = {
+    HTML: "Structure of the Web page",
+    CSS: "Style of the Web page",
+    JavaScript: "Functionality of the Web page",
+    React: "A JavaScript library for building user interfaces",
+    NodeJS: "JavaScript runtime built on Chrome's V8 JavaScript engine"
+}
+
+console.log(dict);
+
+
+/* output: {
+  HTML: 'Structure of the Web page',
+  CSS: 'Style of the Web page',
+  JavaScript: 'Functionality of the Web page',
+  React: 'A JavaScript library for building user interfaces',
+  NodeJS: "JavaScript runtime built on Chrome's V8 JavaScript engine"
+} */
