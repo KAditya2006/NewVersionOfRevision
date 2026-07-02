2217bfbeea3f30a30ab0c52a315a8eca11faaa8f
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-02 09:17:59 +0530

    Solved first chapter fourth problem

---
+++ JSPracticeSet/Chapter1/problem4.js
@@ -0,0 +1,17 @@
+// Try to add a new key to the const object in problem 3. Were you able to do it?
+const obj = {
+    name: "Aditya",
+    age: 19,
+    city: "Darbhanga"
+}
+
+console.log(obj);
+
+obj.country = "India";
+
+console.log(obj);
+
+
+
+/* output: { name: 'Aditya', age: 19, city: 'Darbhanga' }
+{ name: 'Aditya', age: 19, city: 'Darbhanga', country: 'India' } */ 
