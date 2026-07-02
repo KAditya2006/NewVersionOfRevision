f797274012c0de2f185e0ea99a1890a5f081b5c9
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-02 09:53:40 +0530

    Solved Second chapter fifth problem

---
+++ JSPracticeSet/Chapter2/problem5.js
@@ -0,0 +1,8 @@
+//Print "You can drive" or "You can not drive" based on age being greater than 18 using ternary operator.
+
+let age = 32;
+
+let result = (age > 18) ? "You can drive" : "You can not drive";
+console.log(result);
+
+//output: You can drive
