04bb48657331a98f65149e15464e3efecb80f00b
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-03 08:08:53 +0530

    Solved fourth chapter third problem

---
+++ JSPracticeSet/Chapter4/problem3.js
@@ -0,0 +1,7 @@
+// write a program to convert a given string to lowercase
+
+let str = "HELLO, WELCOME TO MY DEVELOPMENT JOURNEY!";
+
+console.log(str.toLowerCase());
+
+//output: hello, welcome to my development journey!
