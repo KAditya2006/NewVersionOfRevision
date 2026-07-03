0339bfffbe9dfd2fbb43cb1523a428e1049b7b9e
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-03 08:18:32 +0530

    Solved fourth chapter fifth problem

---
+++ JSPracticeSet/Chapter4/problem5.js
@@ -0,0 +1,10 @@
+//Try to change the 4th character of a given string "Were you able to do it?"
+
+let str = "Were you able to do it?";
+
+str[3] = "a";
+
+console.log(str);
+
+
+//output: Were you able to do it?
