ea97e6acfb5b5afaed9af36f8622e7360fc5e95e
Aditya kumar Choudhary <kaditya39546@gmail.com>
2026-07-03 08:05:43 +0530

    Solved fourth chapter second problem

---
+++ JSPracticeSet/Chapter4/problem2.js
@@ -0,0 +1,7 @@
+// Explore the includes, startswith, & endswith functions of a string
+
+let str = "Hello, welcome to the world of JavaScript!";
+
+console.log(str.includes("welcome"));
+console.log(str.startsWith("Hello"));
+console.log(str.endsWith("JavaScript!"));
